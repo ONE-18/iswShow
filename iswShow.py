9e5b0e500a542d048167f21c42ab70599e918ccd
Juan Granda <67241136+ONE-18@users.noreply.github.com>
2023-10-23 19:51:47 +0200

    Update iswShow.py
    ups

--- iswShow.py
+++ iswShow.py
@@ -4,13 +4,12 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib
 
-contraseña = "juan6102."
+contraseña = ""
 manual = False
 
 class MenuApp(Gtk.Application):
     def __init__(self):
         global contraseña
-        contraseña = "juan6102."
         super(MenuApp, self).__init__(application_id="org.example.menuapp")
         self.window = None
         self.label = Gtk.Label()
